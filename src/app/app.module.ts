0ebe6b1a89ade5af7c22bcbb508e287b0de32744
Roman <roman.kukurudziak@gmail.com>
2020-11-22 13:39:07 +0200

    forms fully working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,7 +8,7 @@ import { FooterComponent } from './footer/footer.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { FormComponent } from './form/form.component';
-import { FormsModule } from "@angular/forms";
+
 
 @NgModule({
   declarations: [
