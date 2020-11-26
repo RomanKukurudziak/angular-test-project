2bba0bedd12e9694e2f6168d1b90eb33c09eacbb
Roman <roman.kukurudziak@gmail.com>
2020-11-26 14:14:37 +0200

    added popup on subscribe

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,23 +1,31 @@
 import {Component, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class FormComponent {
   @ViewChild('myForm') subscribeForm: NgForm;
 
-  constructor() { }
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
   onSubmit() {
     console.log(this.subscribeForm);
   }
 
   clearForm() {
-    alert('Thank you ' + this.subscribeForm.form.value.username + ' for subscribing with ' + this.subscribeForm.form.value.email + ' adress!');
-    this.subscribeForm.reset();
 
+
+  }
+
+  open(content) {
+    this.modalService.open(content);
   }
 }
