a6d81330b86a7d22c87c275e3cb2454433cc3ce8
Roman <roman.kukurudziak@gmail.com>
2020-12-02 12:12:27 +0200

    added pagination and search

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiService} from '../api.service';
-import { Photo } from './photo';
 import { Post } from './post';
 
 
@@ -16,7 +15,6 @@ export class PostListComponent implements OnInit {
   }
 
   public post: Post[];
-  public photo: Photo[];
   page = 1;
   pageSize = 10;
   searchString = '';
@@ -25,18 +23,16 @@ export class PostListComponent implements OnInit {
     this.apiService.getPosts().subscribe(posts => {
       this.post = posts;
     });
-    this.apiService.getPhotos().subscribe(photos => {
-      this.photo = photos;
-    });
+
   }
 
   search() {
-    if (this.searchString.trim() !== '') {
-      this.post = this.post.filter(res => {
-        return res.body.toLocaleLowerCase().match(this.searchString.toLocaleLowerCase());
+    // if (this.searchString.trim() !== '') {
+      this.post = this.post.filter(post => {
+        return post.body.toLocaleLowerCase().match(this.searchString.toLocaleLowerCase());
       });
 
-    } else if (this.searchString.trim() === '') {
+      if (this.searchString.trim() === '') {
       this.ngOnInit();
       }
     }
